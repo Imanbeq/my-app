1afc397c263394aca0e903c573d8561f85a6ed89
Qlove Iman <imanqlove@gamil.com>
2022-04-19 20:42:40 +0600

    feat:added js code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import Header from "./components/Header";
-import Task1 from "./components/Task1";
+import Header from "./components/Header/Header";
+import BlogEntries from "./components/BlogEntries/BlogEntries";
+import IntroductionMenu from "./components/IntroductionMenu/IntroductionMenu";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
     return (
-        <div className="w3-content" style={{maxWidth:"1400px"}}>
-            <Header/>
-            <Task1/>
-        </div>
+        <>
+            <div className="w3-content" style={{maxWidth: "1400px"}}>
+                <Header/>
+                <div className='w3-row'>
+                    <BlogEntries/>
+                    <IntroductionMenu/>
+                </div>
+            </div>
+            <Footer/>
+        </>
     )
 }
 
