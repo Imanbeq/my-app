b1e2cc87550b5151ee5623b0d6c9c1f5b233abce
Qlove Iman <imanqlove@gamil.com>
2022-04-21 21:10:05 +0600

    feat: added style in Counter.js

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,5 +1,14 @@
 import React, {useState} from 'react';
 
+const styleButton = {
+    backgroundColor: 'black',
+    color:'white',
+    padding:'15px 35px',
+    marginRight: '5px',
+    border:'none',
+    fontSize: '22px'
+}
+
 const Counter = () => {
 
     const [count, setCount] = useState(0);
@@ -14,8 +23,8 @@ const Counter = () => {
 
     return (
         <>
-            <button onClick={increase}>+++</button>
-            <button onClick={decrease}>---</button>
+            <button style={styleButton} onClick={increase}>+++</button>
+            <button style={styleButton} onClick={decrease}>---</button>
             <h1>{count}</h1>
 
         </>
