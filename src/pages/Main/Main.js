f77d2435d275b9893cb7d3aee9b06e4e3d1e85bd
Qlove Iman <imanqlove@gamil.com>
2022-05-17 18:16:21 +0600

    feat: added axios

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,24 +1,48 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 const Main = () => {
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
+
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then( res => {
+                setData(res.data);
+            })
+            .catch( err => {
+                console.log(err);
+            })
+
+        /*fetch('https://jsonplaceholder.typicode.com/users')
             .then((response) => {
                return response.json();
             })
             .then((data) => {
-                const dataArr= data.map((item) => {
-                    return item.name
-                })
-                console.log(dataArr);
-                //useState use!
-            })
+                console.log(data);
+            })*/
     }, [])
+
+
 
     return (
         <>
             <h1>Метод Fetch</h1>
+            <ul>
+                {
+                    data.map(item => (
+                        <li key={item.id}>{item.name}</li>
+                    ))
+                }
+            </ul> <br/>
+
+            <ul>
+                {
+                    data.map(item => (
+                        <li key={item.id}>{item.username}</li>
+                    ))
+                }
+            </ul>
         </>
     );
 };
